1df320ea8baab031dec79207f71c37c43014680d
roberto emanuel <kelvah@gmail.com>
2021-03-31 11:41:15 +0200

    mocked apis call from axios

--- ui-packages/packages/trusty/src/utils/api/httpClient.ts
+++ ui-packages/packages/trusty/src/utils/api/httpClient.ts
@@ -1,4 +1,8 @@
 import axios, { AxiosRequestConfig, CancelTokenSource } from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import generateFakeApis from '../../../api-mock/db';
+
+const mock = new MockAdapter(axios, { delayResponse: 1500 });
 
 export const httpClient = axios.create({
   // @ts-ignore
@@ -22,3 +26,54 @@ export const callOnceHandler = () => {
     return httpClient(config);
   };
 };
+
+const mockedData = generateFakeApis();
+
+mock.onGet('/executions').reply(200, mockedData.executions);
+
+mock.onGet(/\/executions\/decisions\/[A-z0-9-]+$/).reply(config => {
+  const id = config.url.split('/')[3];
+
+  return [
+    200,
+    mockedData.executions.headers.find(
+      execution => execution.executionId === id
+    )
+  ];
+});
+
+mock.onGet(/\/executions\/decisions\/[A-z0-9-]+\/outcomes$/).reply(config => {
+  const id = config.url.split('/')[3];
+
+  return [
+    200,
+    mockedData.outcomes.find(outcome => outcome.header.executionId === id)
+  ];
+});
+
+mock
+  .onGet(/\/executions\/decisions\/[A-z0-9-]+\/outcomes\/[A-z0-9-]+/)
+  .reply(config => {
+    const outcomeId = config.url.split('/')[5];
+    return [
+      200,
+      mockedData.outcomeDetail.find(outcome => outcome.outcomeId === outcomeId)
+    ];
+  });
+
+mock
+  .onGet(/\/executions\/decisions\/[A-z0-9-]+\/explanations\/saliencies/)
+  .reply(() => {
+    return [200, mockedData.saliencies];
+  });
+
+mock
+  .onGet(/\/executions\/decisions\/[A-z0-9-]+\/structuredInputs$/)
+  .reply(() => {
+    return [200, mockedData.inputs];
+  });
+
+mock.onGet(/\/executions\/[A-z0-9-]+\/model$/).reply(config => {
+  const id = config.url.split('/')[2];
+  return [200, mockedData.models.find(model => model.executionId === id)];
+});
