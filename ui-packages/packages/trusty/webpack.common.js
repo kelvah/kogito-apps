8dc56579c547181ef2b700236d3947fc235a5f5c
roberto emanuel <kelvah@gmail.com>
2021-04-15 15:18:16 +0200

    fixing publicPath

--- ui-packages/packages/trusty/webpack.common.js
+++ ui-packages/packages/trusty/webpack.common.js
@@ -90,7 +90,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: ''
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
