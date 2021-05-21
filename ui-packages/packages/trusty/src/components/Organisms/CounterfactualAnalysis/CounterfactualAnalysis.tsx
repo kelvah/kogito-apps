b7f10eb4429ecf841f573a0795b1c681da386f6d
roberto emanuel <kelvah@gmail.com>
2021-05-21 11:31:50 +0200

    demo: allowing entering constraints only for selected inputs

--- ui-packages/packages/trusty/src/components/Organisms/CounterfactualAnalysis/CounterfactualAnalysis.tsx
+++ ui-packages/packages/trusty/src/components/Organisms/CounterfactualAnalysis/CounterfactualAnalysis.tsx
@@ -68,16 +68,11 @@ const CounterfactualAnalysis = (props: CounterfactualAnalysisProps) => {
   const areInputsSelected = (inputs: CFSearchInput[]) => {
     // filtering all non fixed inputs
     const selectedInputs = inputs.filter(domain => domain.isFixed === false);
-    // checking if all inputs have a domain specified, with the exception of
-    // boolean (do not require one) and structured inputs (not yet supported)
+    // checking if all inputs have a domain specified (with the exception of
+    // boolean, which do not require one)
     return (
       selectedInputs.length > 0 &&
-      inputs.every(
-        input =>
-          input.domain ||
-          input.components !== null ||
-          typeof input.value === 'boolean'
-      )
+      selectedInputs.every(input => input.domain || input.typeRef === 'boolean')
     );
   };
 
